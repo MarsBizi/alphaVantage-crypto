3420260d68b4cee5541c88dc21dfaea8ed28ba42
marsbizi <marsbizi@gmail.com>
2022-01-28 20:16:03 -0700

    Initial commit

---
+++ av_api.py
@@ -0,0 +1,20 @@
+#%%
+import requests
+import pandas as pd
+
+def get_crypto_price(symbol, exchange, start_date = None):
+    api_key = 'insert_api_key'
+    api_url = f'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol={symbol}&market={exchange}&apikey={api_key}'
+    raw_df = requests.get(api_url).json()
+    df = pd.DataFrame(raw_df['Time Series (Digital Currency Daily)']).T
+    df = df.rename(columns = {'1a. open (USD)': 'open', '2a. high (USD)': 'high', '3a. low (USD)': 'low', '4a. close (USD)': 'close', '5. volume': 'volume'})
+    for i in df.columns:
+        df[i] = df[i].astype(float)
+    df.index = pd.to_datetime(df.index)
+    df = df.iloc[::-1].drop(['1b. open (USD)', '2b. high (USD)', '3b. low (USD)', '4b. close (USD)', '6. market cap (USD)'], axis = 1)
+    if start_date:
+        df = df[df.index >= start_date]
+    return df
+
+btc = get_crypto_price(symbol = 'BTC', exchange = 'USD', start_date = '2020-01-01')
+btc
